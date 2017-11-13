c5499cc05fa83fbc98a0768a3a792a55dcc6e7b2
Daniela <t.dani@live.com>
2017-11-13 11:47:50 -0800

    nav menus

--- wp-content/themes/learningwordpress/functions.php
+++ wp-content/themes/learningwordpress/functions.php
@@ -1,8 +1,14 @@
-<?php 
+<?php
 
 function learningWordPress_Resources(){
-    
+
     wp_enqueue_style('style', get_stylesheet_uri());
 }
 
 add_action('wp_enqueue_scripts', 'learningWordPress_resources');
+
+// Navigation Menus
+register_nav_menus( array(
+  'primary' => __('Primary Menu'),
+  'footer' => __('Footer Menu'),
+));
